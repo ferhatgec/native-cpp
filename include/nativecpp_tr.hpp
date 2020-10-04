ca9d64ec0fd36aa698b1ea2114fff5dcd51f44e5
ferhatgec <ferhatgectao@gmail.com>
2020-10-05 01:03:50 +0300

    Add source.

---
+++ include/nativecpp_tr.hpp
@@ -0,0 +1,40 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+/*
+	For Turkish language.
+*/
+
+#ifndef NATIVE_CPP_TR_HPP
+#define NATIVE_CPP_TR_HPP
+
+#include <iostream>
+
+/* Character */
+typedef char karakter;
+
+/* Integer */
+typedef int tam;
+
+/* Floating point, decimal */
+typedef float ondalik;
+typedef double cift;
+typedef long double uzuncift;
+typedef std::string dizgi;
+
+/* Boolean algebra */
+typedef bool boole;
+
+/* Logical operators */
+#define ve (and)
+#define ya_da (or)
+
+/* Main function */
+#define ana (main)
+#define const sabit
+
+#endif /* NATIVECPP_TR_HPP */
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include "include/nativecpp_tr.hpp"
+#include <iostream>
+
+tam ana() {
+  printf("Merhaba dunya!");
+}
